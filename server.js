5bfa79e758d1d2572f98a8b0e787c986e23a7bfe
unknown <naolson.1819@gmail.com>
2019-04-18 16:29:41 -0500

    added second dash

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(_dirname, 'hello-vue', 'dist')));
+app.use(express.static(path.join(__dirname, 'hello-vue', 'dist')));
 
 app.use('/api', routes);
 
 var server = app.listen(process.env.PORT || 3000, function () {
     console.log('app running on port', server.address().port)
 });
+
